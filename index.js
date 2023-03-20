e5d5d060a56463cae864b37985bcedbfb4d53d97
Gabriel Sargent <gabe2sargent@gmail.com>
2023-03-20 17:37:11 -0500

    objlab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution in this file!
+const employee = {
+    name: 'Mike',
+    streetAddress: '11 Broadway',
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+};
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee.streetAddress = "12 Broadway";
+    return employee;
+
+};
+function deleteFromEmployeeByKey (employee, key) {
+    const newEmployee = {employee};
+    employee[key] = 'Sam';
+
+    return newEmployee;
+
+};
+function destructivelyDeleteFromEmployeeByKey (employee, key) {
+    delete employee[key];
+    return employee;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
